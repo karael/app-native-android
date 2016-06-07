0c628a741dba0216c43a54d77e876a3ca6147eab
karael <owickarael@gmail.com>
2016-06-07 02:45:24 +0200

    bugged game toolbar

---
+++ app/src/main/java/com/zstudio/app/cinemovie/GameActivity.java
@@ -0,0 +1,38 @@
+package com.zstudio.app.cinemovie;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.game_toolbar);
+        setSupportActionBar(toolbar);
+        // toolbar.setNavigationIcon(R.drawable.back);
+        // toolbar.setLogo(R.drawable.ic_launcher);
+        //toolbar.setTitle("Title");
+        // toolbar.setSubtitle("Subtitle");
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    public void onGroupItemClick(MenuItem item) {
+        // One of the group items (using the onClick attribute) was clicked
+        // The item parameter passed here indicates which item it is
+        // All other menu item clicks are handled by onOptionsItemSelected()
+    }
+
+}
