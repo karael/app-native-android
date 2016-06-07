240551c12368e5b80120273306e008251ee2e2bc
karael <owickarael@gmail.com>
2016-06-07 02:49:55 +0200

    removing toolbar

--- app/src/main/java/com/zstudio/app/cinemovie/GameActivity.java
+++ app/src/main/java/com/zstudio/app/cinemovie/GameActivity.java
@@ -14,25 +14,6 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.game_toolbar);
-        setSupportActionBar(toolbar);
-        // toolbar.setNavigationIcon(R.drawable.back);
-        // toolbar.setLogo(R.drawable.ic_launcher);
-        //toolbar.setTitle("Title");
-        // toolbar.setSubtitle("Subtitle");
-    }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    public void onGroupItemClick(MenuItem item) {
-        // One of the group items (using the onClick attribute) was clicked
-        // The item parameter passed here indicates which item it is
-        // All other menu item clicks are handled by onOptionsItemSelected()
     }
 
 }
