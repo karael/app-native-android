c8d33acdfb51bb3e8af6ad31be6724f911bf435a
raphael aupee <raphael.aupee@hetic.net>
2016-06-09 15:52:15 +0200

    Add permission request to get UUID

--- app/src/main/java/com/zstudio/app/cinemovie/MainActivity.java
+++ app/src/main/java/com/zstudio/app/cinemovie/MainActivity.java
@@ -1,5 +1,9 @@
 package com.zstudio.app.cinemovie;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,18 +18,54 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //checking permission
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_PHONE_STATE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_PHONE_STATE)) {
+                //showing something?
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_PHONE_STATE},
+                        PERMISSIONS_REQUEST_READ_PHONE_STATE);
+            }
+        }
+
         setContentView(R.layout.activity_home);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_READ_PHONE_STATE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+
+                }
+            }
+        }
     }
 
 
     public void onClickPlayButton(View view) {
-        Intent myTriggerActivityIntent = new Intent(this, GameActivity.class);
-        startActivity(myTriggerActivityIntent);
+        startActivity(new Intent(CineApplication.getSingleInstance(), GameActivity.class));
+
     }
 
     public void onClickAchievementButton(View view) {
-        Intent myTriggerActivityIntent = new Intent(this, GameActivity.class);
-        startActivity(myTriggerActivityIntent);
+        startActivity(new Intent(this, GameActivity.class));
+
     }
 
 }
